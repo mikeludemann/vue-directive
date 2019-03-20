220391a2ee1b9c1cfa1816efb6bbffb2ac05e8dc
Mike Ludemann <mike-ludemann@hotmail.de>
2019-03-20 16:57:39 +0100

    Toggle click animation helper

---
+++ src/directive/event-handling/clickAnimation.js
@@ -0,0 +1,35 @@
+Vue.directive('animation', {
+	bind(el, binding, vnode) {
+		const bvalue = binding.value;
+		const elemStyle = el.style;
+
+        var originalWidth = elemStyle.width;
+        var originalHeight = elemStyle.height;
+        var originalTextSize = elemStyle.fontSize;
+
+		el.addEventListener('click', () => {
+                let active = vnode.context.mode_active;
+
+                active = !active;
+
+                vnode.context.mode_active = active;
+
+                if (!active) {
+                    elemStyle.width = bvalue.width;
+                    elemStyle.height = bvalue.height;
+                    elemStyle.fontSize = bvalue.fontSize;
+                    elemStyle.transition = "all 1s ease-in-out";
+                } else {
+                    elemStyle.width = originalWidth;
+                    elemStyle.height = originalHeight;
+                    elemStyle.fontSize = originalTextSize;
+                    elemStyle.transition = "all 1s ease-in-out";
+                }
+            },
+            false
+        );
+	},
+    unbind(el, binding, vnode) {
+        el.removeEventListener('click');
+    }
+}); 
